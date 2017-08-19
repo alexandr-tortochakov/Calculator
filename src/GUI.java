d9eb7d3c3b19bba5967b000a42837694a4597358
alex <tortochakov27@gmail.com>
2017-08-19 17:31:19 +0700

    refactoring

--- src/GUI.java
+++ src/GUI.java
@@ -5,8 +5,8 @@ import java.awt.event.ActionListener;
 
 public class GUI {
     JFrame frmCalculator;
-    JTextField displayArea;
-    StringBuilder builder = new StringBuilder();
+    JTextField displayField;
+    StringBuilder inputAccumulator = new StringBuilder();
     boolean equalWasPressed = false;
 
 
@@ -22,100 +22,100 @@ public class GUI {
         frmCalculator.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frmCalculator.getContentPane().setLayout(null);
 
-        displayArea = new JTextField();
-        displayArea.setHorizontalAlignment(SwingConstants.RIGHT);
-        displayArea.setEditable(false);
-        displayArea.setBounds(10, 10, 215, 40);
-        frmCalculator.getContentPane().add(displayArea);
-        displayArea.setColumns(10);
+        displayField = new JTextField();
+        displayField.setHorizontalAlignment(SwingConstants.RIGHT);
+        displayField.setEditable(false);
+        displayField.setBounds(10, 10, 215, 40);
+        frmCalculator.getContentPane().add(displayField);
+        displayField.setColumns(10);
 
         JButton btn7 = new JButton("7");
-        btn7.addActionListener(new DigitButton(builder, displayArea, '7'));
+        btn7.addActionListener(new DigitButton(displayField, '7'));
         btn7.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn7.setBounds(10, 60, 50, 50);
         frmCalculator.getContentPane().add(btn7);
 
         JButton btn8 = new JButton("8");
-        btn8.addActionListener(new DigitButton(builder, displayArea, '8'));
+        btn8.addActionListener(new DigitButton(displayField, '8'));
         btn8.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn8.setBounds(65, 60, 50, 50);
         frmCalculator.getContentPane().add(btn8);
 
         JButton btn9 = new JButton("9");
-        btn9.addActionListener(new DigitButton(builder, displayArea, '9'));
+        btn9.addActionListener(new DigitButton(displayField, '9'));
         btn9.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn9.setBounds(120, 60, 50, 50);
         frmCalculator.getContentPane().add(btn9);
 
         JButton btn6 = new JButton("6");
-        btn6.addActionListener(new DigitButton(builder, displayArea, '6'));
+        btn6.addActionListener(new DigitButton(displayField, '6'));
         btn6.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn6.setBounds(10, 115, 50, 50);
         frmCalculator.getContentPane().add(btn6);
 
         JButton btn5 = new JButton("5");
-        btn5.addActionListener(new DigitButton(builder, displayArea, '5'));
+        btn5.addActionListener(new DigitButton(displayField, '5'));
         btn5.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn5.setBounds(65, 115, 50, 50);
         frmCalculator.getContentPane().add(btn5);
 
         JButton btn4 = new JButton("4");
-        btn4.addActionListener(new DigitButton(builder, displayArea, '4'));
+        btn4.addActionListener(new DigitButton(displayField, '4'));
         btn4.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn4.setBounds(120, 115, 50, 50);
         frmCalculator.getContentPane().add(btn4);
 
         JButton btn3 = new JButton("3");
-        btn3.addActionListener(new DigitButton(builder, displayArea, '3'));
+        btn3.addActionListener(new DigitButton(displayField, '3'));
         btn3.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn3.setBounds(10, 170, 50, 50);
         frmCalculator.getContentPane().add(btn3);
 
         JButton btn2 = new JButton("2");
-        btn2.addActionListener(new DigitButton(builder, displayArea, '2'));
+        btn2.addActionListener(new DigitButton(displayField, '2'));
         btn2.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn2.setBounds(65, 170, 50, 50);
         frmCalculator.getContentPane().add(btn2);
 
         JButton btn1 = new JButton("1");
-        btn1.addActionListener(new DigitButton(builder, displayArea, '1'));
+        btn1.addActionListener(new DigitButton(displayField, '1'));
         btn1.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn1.setBounds(120, 170, 50, 50);
         frmCalculator.getContentPane().add(btn1);
 
         JButton btnPlus = new JButton("+");
-        btnPlus.addActionListener(new OperationSign(displayArea, '+', builder));
+        btnPlus.addActionListener(new OperationSign(displayField, '+'));
         btnPlus.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btnPlus.setBounds(175, 60, 50, 50);
         frmCalculator.getContentPane().add(btnPlus);
 
         JButton btnMinus = new JButton("-");
-        btnMinus.addActionListener(new OperationSign(displayArea, '-', builder));
+        btnMinus.addActionListener(new OperationSign(displayField, '-'));
         btnMinus.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btnMinus.setBounds(175, 115, 50, 50);
         frmCalculator.getContentPane().add(btnMinus);
 
         JButton btnDivision = new JButton("/");
-        btnDivision.addActionListener(new OperationSign(displayArea, '/', builder));
+        btnDivision.addActionListener(new OperationSign(displayField, '/'));
         btnDivision.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btnDivision.setBounds(175, 170, 50, 50);
         frmCalculator.getContentPane().add(btnDivision);
 
         JButton btn0 = new JButton("0");
-        btn0.addActionListener(new DigitButton(builder, displayArea, '0'));
+        btn0.addActionListener(new DigitButton(displayField, '0'));
         btn0.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btn0.setBounds(10, 225, 50, 50);
         frmCalculator.getContentPane().add(btn0);
 
         JButton btnComma = new JButton(".");
-        btnComma.addActionListener(new OperationSign(displayArea, '.', builder));
+        btnComma.addActionListener(new OperationSign(displayField, '.'));
         btnComma.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btnComma.setBounds(65, 225, 50, 50);
         frmCalculator.getContentPane().add(btnComma);
 
         JButton btnEqualsSign = new JButton("=");
         btnEqualsSign.addActionListener(e -> {
-            String displayText = displayArea.getText();
+            String displayText = displayField.getText();
             StringBuilder stringToDigit = new StringBuilder();
             double firstDigit = 0;
             char operSign = 0;
@@ -137,29 +137,29 @@ public class GUI {
             double secondDigit = Double.parseDouble(stringToDigit.toString());
             stringToDigit.setLength(0);
             Calculator calculator = new Calculator();
-            double temp = 0;
+            double operationResult = 0;
             switch (operSign) {
                 case '+':
-                    temp = calculator.addition(firstDigit, secondDigit);
+                    operationResult = calculator.addition(firstDigit, secondDigit);
                     break;
                 case '-':
-                    temp = calculator.subtraction(firstDigit, secondDigit);
+                    operationResult = calculator.subtraction(firstDigit, secondDigit);
                     break;
                 case '/':
-                    temp = calculator.division(firstDigit, secondDigit);
+                    operationResult = calculator.division(firstDigit, secondDigit);
                     break;
                 case '*':
-                    temp = calculator.multiplication(firstDigit, secondDigit);
+                    operationResult = calculator.multiplication(firstDigit, secondDigit);
                     break;
             }
-            double fractionalPart = temp - (int) temp;
-            String tempString;
-            if (fractionalPart < 1.e-8) {
-                tempString = String.valueOf((int) temp);
-            } else tempString = String.valueOf(temp);
-            displayArea.setText(tempString);
-            builder.setLength(0);
-            builder.append(tempString);
+            double fractionalPart = operationResult - (int) operationResult;
+            String stringForOutput;
+            if (fractionalPart == 0) {
+                stringForOutput = String.valueOf((int) operationResult);
+            } else stringForOutput = String.valueOf(operationResult);
+            displayField.setText(stringForOutput);
+            inputAccumulator.setLength(0);
+            inputAccumulator.append(stringForOutput);
             equalWasPressed = true;
         });
         btnEqualsSign.setFont(new Font("Sitka Text", Font.BOLD, 15));
@@ -167,7 +167,7 @@ public class GUI {
         frmCalculator.getContentPane().add(btnEqualsSign);
 
         JButton btnMultiply = new JButton("*");
-        btnMultiply.addActionListener(new OperationSign(displayArea, '*', builder));
+        btnMultiply.addActionListener(new OperationSign(displayField, '*'));
         btnMultiply.setFont(new Font("Sitka Text", Font.BOLD, 15));
         btnMultiply.setBounds(175, 225, 50, 50);
         frmCalculator.getContentPane().add(btnMultiply);
@@ -181,12 +181,10 @@ public class GUI {
     }
 
     public class DigitButton implements ActionListener {
-        StringBuilder builder;
         JTextField field;
         char digit;
 
-        public DigitButton(StringBuilder builder, JTextField field, char digit) {
-            this.builder = builder;
+        public DigitButton(JTextField field, char digit) {
             this.field = field;
             this.digit = digit;
         }
@@ -194,23 +192,21 @@ public class GUI {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (equalWasPressed) {
-                builder.setLength(0);
+                inputAccumulator.setLength(0);
                 equalWasPressed = false;
             }
-            builder.append(digit);
-            field.setText(builder.toString());
+            inputAccumulator.append(digit);
+            field.setText(inputAccumulator.toString());
         }
     }
 
     public class OperationSign implements ActionListener {
         JTextField displayArea;
         char sign;
-        StringBuilder builder;
 
-        public OperationSign(JTextField displayArea, char sign, StringBuilder builder) {
+        public OperationSign(JTextField displayArea, char sign) {
             this.displayArea = displayArea;
             this.sign = sign;
-            this.builder = builder;
         }
 
         @Override
@@ -219,14 +215,14 @@ public class GUI {
             if (displayArea.getText().length() == 0 && sign != '-') {
                 return;
             } else if (displayArea.getText().length() == 0 && sign == '-') {
-                builder.append(sign);
-                displayArea.setText(builder.toString());
+                inputAccumulator.append(sign);
+                displayArea.setText(inputAccumulator.toString());
                 return;
             }
             char lastChar = displayArea.getText().charAt(displayArea.getText().length() - 1);
             if (Character.isDigit(lastChar) ) {
-                builder.append(sign);
-                displayArea.setText(builder.toString());
+                inputAccumulator.append(sign);
+                displayArea.setText(inputAccumulator.toString());
             }
         }
     }
